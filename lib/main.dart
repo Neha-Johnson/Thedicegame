30976e1e01865d48199f26b66692f4b07040c07c
Neha-Johnson <johnsonneha97@gmail.com>
2026-03-14 14:04:30 +0530

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,58 +2,77 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: const Color.fromARGB(255, 7, 17, 15),
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-    ),
-    body: DicePage(),
-    ),
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.tealAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Center(
+            child: Text("Dice App"),
+          ) 
+        ),
+        body: const DicePage(),
+      ),
+    );
+  }
+}
+
+// Stateful Widget (Dice logic)
 class DicePage extends StatefulWidget {
   const DicePage({super.key});
+
   @override
   State<DicePage> createState() => _DicePageState();
 }
+
 class _DicePageState extends State<DicePage> {
 
-  int diceNumber = 4;
+   int diceNumber = 4;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
-          'Dice Number: $diceNumber',
-          style: TextStyle(fontSize: 30),
-        ),
-SizedBox(
-  width: 150.0,
-  child: TextButton(
-    style: TextButton.styleFrom(
-      foregroundColor: Colors.tealAccent
-    ),
-    onPressed: () {
-      setState(() {
-        print('$diceNumber pressed');
-        diceNumber = Random().nextInt(6) + 1;
-        print('dice number is $diceNumber');
-        
-      });
-    },
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image(
-          image: AssetImage('images/dice$diceNumber.png'),
-        ),
-      ],
-    ),
-  ),
-),
+            'Dice Number: $diceNumber',
+            style: const TextStyle(fontSize: 30),
+          ),
+      
+          SizedBox(
+            width: 150.0,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.tealAccent,
+              ),
+              onPressed: () {
+                setState(() {
+                  print('$diceNumber pressed');
+                  diceNumber = Random().nextInt(6) + 1;
+                  print('dice number is $diceNumber');
+                });
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image:AssetImage('images/dice$diceNumber.png'),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
